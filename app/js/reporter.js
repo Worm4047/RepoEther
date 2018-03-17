1b90b656569d76aa5e6c5cf0185a031b0735df83
Worm4047 <abhidagar1@gmail.com>
2018-03-17 19:44:29 +0530

    Asdas

--- app/js/reporter.js
+++ app/js/reporter.js
@@ -1,6 +1,6 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
-// import "../css/main.css";
+// import "../stylesheets/app.css";
+import "../css/main.css";
 import "../css/util.css";
 import "../css/dashboard.css";
 
@@ -17,15 +17,21 @@ var Reporter = contract(reporter_artifacts);
 
 window.registerComplaint = function(form) {
   console.log(form);
-  let title = $('#title').val();
-  let visibility = parseInt($("#visibility").val());
-  let type = parseInt($("#type_of_crime").val());
-  let admin = $("#admin").val();
+  let complaint = $('#complaint').val();
+  let contact_info = parseInt($('#contact_info').val());
+ // let crime_date = 
+  let type_of_complaint = parseInt($('#type_of_complaint').val());
+  let complaint_visibility = $('#complaint_visibility').val() == 'on'?1:0;
+  let crime_time = $('#crime_date').val();
+  let location = 'allahabad'; 
+  let admin = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57';
+  console.log(type_of_complaint, complaint_visibility, admin, complaint, contact_info, crime_time);
   // console.log(title, typeof(visibility), typeof(type), admin);
   // console.log(web3.eth.accounts[0]);
+  //uint type_of_complaint, uint visibility,  address admin, bytes32 complaint, bytes32 contact_info, bytes32 crime_time, bytes32 location
   Reporter.deployed().then(function(contractInstance){
     console.log(contractInstance);
-    contractInstance.register_complaint(type, visibility, admin, title,{gas: 1400000, from: web3.eth.accounts[0]})
+    contractInstance.register_complaint(type_of_complaint, complaint_visibility, complaint, contact_info, crime_time,location,{gas: 1400000, from: web3.eth.accounts[0]})
     .then(function(){
       console.log("Complaint registered");
     })
@@ -34,10 +40,12 @@ window.registerComplaint = function(form) {
     })
   })
 }
+
 window.displayComplaints = function(){
+
   console.log("HEllo");
   let $div = $("#div");
-  let fields = ['id','title','contact_info','type_of_complaint','visibility'];
+  let fields = [ 'admin', 'title', 'contact_info', 'type_of_complaint', 'visibility', 'crime_time'];
   let complaints = {};
   Reporter.deployed().then(function(contractInstance){
     contractInstance.get_all_complaints.call()
@@ -48,11 +56,9 @@ window.displayComplaints = function(){
         for(let j=0;j<fields.length;j++){
 
           obj[fields[j]] = res[j][i].toString();
-          if ( j==1 || j==2)
-          {
+            if(j==5 || j==1)
             obj[fields[j]] = web3.toAscii(obj[fields[j]]);
-          }
-          //console.log(i,j, obj[fields[j]]);
+
         }
         complaints[i]=obj;
       }
@@ -69,13 +75,11 @@ $( document ).ready(function() {
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.warn("No web3 detected. Falling back to http://localhost:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
   }
 
   Reporter.setProvider(web3.currentProvider);
   console.log(web3.currentProvider);
-
-
 });
