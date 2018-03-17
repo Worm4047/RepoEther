829af785bb48f4ef9fa7b6e63caf1b2c55d4bc01
Worm4047 <abhidagar1@gmail.com>
2018-03-17 14:42:01 +0530

    Setup

--- contracts/Reporter.sol
+++ contracts/Reporter.sol
@@ -1,150 +1,82 @@
 pragma solidity ^0.4.18;
+pragma experimental ABIEncoderV2;
 
 contract Reporter {
 
-	struct Stake {
-		bytes32 user_address;
-		uint stake;
-		uint opinion; //1->genuine FIR, 0->fake FIR
-	}
-
     struct Complaint {
         uint id;
-        bytes32 admin; //uportId of person registering complaint.
+        address admin; //adhaar card of person registering complaint.
         bytes32 complaint;
         bytes32 contact_info;
-        bytes32 tags;
-        bytes32 proposed_solution;
-        bytes32 documents;
+        string address_info;
+        string proposed_solution;
+        // bytes32 documents;
         uint status;//0-> pending/open 1-> in process 2->closed/resolved
-        uint upvotes;
+        uint256 upvotes;
+        bytes32 solved_by; //adhaar_card of person who solved it
         bytes32 location;//In the form of longitude latitude
         bytes32 date_of_crime;
         uint visibility;//0 -> closed 1-> open
         uint type_of_complaint;// 0-> criminal cases eg(Theft) 1->civil cases( Consumer court )2->enforcement( debt issue )
-        // uint256 time;
+        bytes32 crime_time;
     }
+    //Mapping of adhaar card to array of complaint id;
+    mapping(address => uint[]) public solved_complaints;
 
+    //Mapping of adhaar card to array of lodged solved_complaints
+    //To retreive complaints a person has lodged
+    mapping(address => uint[]) public lodged_complaints;
 
-	//Mapping of uportid to an integer array of complaint ids
-	mapping(bytes32 => uint[]) my_complaints;
+    //All complaints
+    Complaint[] all_complaints;
 
-	//Array of all_complaints
-	Complaint[] all_complaints;
-
-	//Mapping of uportid to and integer array of complaint ids
-	mapping(bytes32 => uint[]) my_solved_complaints;
-
-	mapping(uint => Stake[]) stakes_on_complaint;
-
-	function registerComplaints(bytes32 admin, bytes32 complaint, bytes32 contact_info, bytes32 tags, bytes32 documents, bytes32 location, bytes32 date_of_crime, uint visibility, uint type_of_complaint ) public payable {
-		Complaint memory newcomplaint;
-		newcomplaint.id = all_complaints.length + 1;
-        newcomplaint.admin = admin;
-        newcomplaint.complaint = complaint;
-        newcomplaint.contact_info = contact_info;
-        newcomplaint.tags = tags;
-        newcomplaint.documents = documents;
-        newcomplaint.location = location;
-        newcomplaint.date_of_crime = date_of_crime;
-        newcomplaint.visibility = visibility;
-        newcomplaint.type_of_complaint = type_of_complaint;
-        newcomplaint.proposed_solution = '';
-        newcomplaint.status = 0;//pending;
-        newcomplaint.upvotes = 0;
-	} 
-
-    function getAllComplaints1() public constant returns (uint[], bytes32[], bytes32[],bytes32[],bytes32[],bytes32[]){
-        
+    //Getter for all_complaints
+    function get_all_complaints() public returns (uint[], address[], bytes32[],bytes32[], uint[], uint[], bytes32[]){
         uint[] memory id = new uint[](all_complaints.length);
-        bytes32[] memory admin = new bytes32[](all_complaints.length);
-        bytes32[] memory complaint = new bytes32[](all_complaints.length);
+        bytes32[] memory title = new bytes32[](all_complaints.length);
         bytes32[] memory contact_info = new bytes32[](all_complaints.length);
-        bytes32[] memory tags = new bytes32[](all_complaints.length);
-        bytes32[] memory documents = new bytes32[](all_complaints.length);
-
+        uint[] memory type_of_complaint = new uint[](all_complaints.length);
+        uint[] memory visibility = new uint[](all_complaints.length);
+        bytes32[] memory location = new bytes32[](all_complaints.length);
+        // bytes32[] memory crime_time = new bytes32[](all_complaints.length);
+        address[] memory admin = new address[](all_complaints.length);
         for(uint i=0;i<all_complaints.length;i++){
             Complaint storage c = all_complaints[i];
             id[i] = c.id;
-            admin[i] = c.admin;
-            complaint[i] = c.complaint;
+            title[i] = c.complaint;
             contact_info[i] = c.contact_info;
-            tags[i] = c.tags;
-            documents[i] = c.documents;
-
-        }
-
-        return(id, admin, complaint, contact_info, tags, documents);
-    }
-    function getAllComplaints2() public constant returns (bytes32[],bytes32[], uint[], uint[], bytes32[], uint[]){
-        
-        bytes32[] memory location = new bytes32[](all_complaints.length);
-        bytes32[] memory date_of_crime = new bytes32[](all_complaints.length);
-        uint[] memory visibility = new uint[](all_complaints.length);
-        uint[] memory type_of_complaint = new uint[](all_complaints.length);
-        bytes32[] memory proposed_solution = new bytes32[](all_complaints.length);
-        uint[] memory status = new uint[](all_complaints.length);
-
-        for(uint i=0;i<all_complaints.length;i++){
-            Complaint storage c = all_complaints[i];
-
-            location[i] = c.location;
-            date_of_crime[i] = c.date_of_crime;
-            visibility[i] = c.visibility;
             type_of_complaint[i] = c.type_of_complaint;
-            proposed_solution[i] = c.proposed_solution;
-            status[i] = c.status;
+            visibility[i] = c.visibility;
+            location[i] = c.location;
+            // crime_time[i] = c.crime_time;
         }
 
-        return(location, date_of_crime, visibility, type_of_complaint, proposed_solution, status);
+        return(id, admin, title, contact_info, type_of_complaint, visibility, location);
     }
-	function acceptComplaint(bytes32 police_station, uint complaint_id) public payable{
-		my_complaints[police_station].push(complaint_id);
-		all_complaints[complaint_id].status = 1; //in progress
-	}
+    
+// uint id,string documents,uint type_of_complaint, uint visibility,  bytes32 admin, string title, string contact_info, string address_info, uint256 time, string location
+    function register_complaint(uint type_of_complaint, uint visibility,  address admin, bytes32 complaint, bytes32 contact_info, bytes32 crime_time, bytes32 location) public{
+        Complaint memory newcomplaint;
+        uint id = all_complaints.length+1;
+        newcomplaint.id = id;
+        newcomplaint.admin = admin;
+        newcomplaint.complaint = bytes32(complaint);
+        newcomplaint.contact_info = contact_info;
+        newcomplaint.proposed_solution = '';
+        newcomplaint.status = 0;
+        newcomplaint.upvotes = 0;
+        newcomplaint.solved_by = '';
+        // newcomplaint.time = time;
+        newcomplaint.location = bytes32(location);
+        newcomplaint.type_of_complaint = type_of_complaint;
+        newcomplaint.visibility = visibility;
+        newcomplaint.crime_time = crime_time;
 
-	function proposeSolution(uint complaint_id, bytes32 proposed_solution) public payable{
-		all_complaints[complaint_id].proposed_solution = proposed_solution;
-	}
+        //Add this complaint to array of lodged complaints by the adhaar number
+        lodged_complaints[admin].push(id);
+        //Add this complaint to array of all complaints
+        all_complaints.push(newcomplaint);
 
-	function acceptSolution(uint complaint_id, bytes32 police_station) public  payable{
-		all_complaints[complaint_id].status = 2;// closed;
-		my_solved_complaints[police_station].push(complaint_id);
-		distributeFunds(complaint_id, 1);//it was a genuine problem
-	}
+    }
 
-	function addStake(uint complaint_id, bytes32 user_address, uint stake, uint opinion) public payable {
-		Stake memory newstake;
-		newstake.user_address = user_address;
-		newstake.stake = stake;
-		newstake.opinion = opinion;
-		stakes_on_complaint[complaint_id].push(newstake);
-	}
-
-	function returnStakeValue(uint complaint_id) public returns(uint){
-		uint total=0;
-		for(uint i=0;i< stakes_on_complaint[complaint_id].length;i++){
-			total += stakes_on_complaint[complaint_id][i].stake;
-		}
-		return total;
-	}
-
-	function distributeFunds(uint complaint_id, uint result) public payable {
-		uint total_return_amount=0;
-		uint wrong_opinion_count=0;
-		for(uint i=0;i<stakes_on_complaint[complaint_id].length;i++){
-			if(stakes_on_complaint[complaint_id][i].opinion != result){
-				total_return_amount += stakes_on_complaint[complaint_id][i].stake;
-				wrong_opinion_count++;
-			}
-		}
-		uint extra_return = total_return_amount/(stakes_on_complaint[complaint_id].length+wrong_opinion_count);
-		for(i=0;i<stakes_on_complaint[complaint_id].length;i++){
-			if(stakes_on_complaint[complaint_id][i].opinion != result)
-				continue;
-			uint amount = stakes_on_complaint[complaint_id][i].stake + extra_return;
-			address to_address = address(stakes_on_complaint[complaint_id][i].user_address);
-			to_address.transfer(amount);			
-		}
-	}
 }
